d6e0284852888751698dc2d79c2b11fe38b16373
P01son6415 <liyudong123@hotmail.com>
2023-07-29 17:28:34 +0800

    update

--- eval/evaluate_ceval_original.py
+++ eval/evaluate_ceval_original.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
                 src_tensor, seg_tensor = torch.LongTensor([src]).to(device), torch.LongTensor([seg]).to(device)
 
                 output = model(src_tensor)
-                print(output[0].size())
+                print(output[0][-1].size())
 
                 next_token_logits = F.softmax(output[0][-1])
 
