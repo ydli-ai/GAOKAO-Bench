3bae6bbc4a198e8efd68c7788088a366e8826bb4
P01son6415 <liyudong123@hotmail.com>
2023-07-29 15:37:09 +0800

    update

--- Bench/openai.py
+++ Bench/openai.py
@@ -21,7 +21,7 @@ class  OpenaiAPI:
         """
         prompt = f"### Instruction:{prompt + question}  ### Response:"
         inputs = self.tokenizer(prompt, return_tensors="pt").to("cuda:0")
-        generate_ids = self.model.generate(inputs.input_ids, do_sample=True, max_new_tokens=1024, top_k=10, top_p=0.85, temperature=1, repetition_penalty=1.15, eos_token_id=2, bos_token_id=1, pad_token_id=0)
+        generate_ids = self.model.generate(inputs.input_ids, do_sample=True, max_new_tokens=768, top_k=10, top_p=0.85, temperature=1, repetition_penalty=1.15, eos_token_id=2, bos_token_id=1, pad_token_id=0)
         response = self.tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
         response = response.lstrip(prompt)
 
